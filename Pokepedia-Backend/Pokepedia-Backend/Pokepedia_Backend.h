6ff028d32f141117dc23e820e82c9a808ca7eb14
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-05-30 09:44:47 -0700

    project init

---
+++ Pokepedia-Backend/Pokepedia-Backend/Pokepedia_Backend.h
@@ -0,0 +1,18 @@
+//
+//  Pokepedia_Backend.h
+//  Pokepedia-Backend
+//
+//  Created by Василий Клецкин on 5/30/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Pokepedia_Backend.
+FOUNDATION_EXPORT double Pokepedia_BackendVersionNumber;
+
+//! Project version string for Pokepedia_Backend.
+FOUNDATION_EXPORT const unsigned char Pokepedia_BackendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pokepedia_Backend/PublicHeader.h>
+
+
